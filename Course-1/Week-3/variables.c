2f5d7674d349e08fdaf56a2cdec47c282152eb42
Cassia <kklyra@gmail.com>
2024-06-28 17:32:34 +0200

    Changing comment

--- Course-1/Week-3/variables.c
+++ Course-1/Week-3/variables.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main () {
     //DECLARATION AND DEFINITION
-    int balance = 50;
+    int balance;
+    balance = 50;
     //USE
     printf("I have %d dollars in my account\n", balance);
     return 0;
