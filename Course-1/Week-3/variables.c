58e282d60fa90cc1aa5f85a51d7ba7310b936092
Cassia <kklyra@gmail.com>
2024-06-30 19:14:37 +0200

    names dos and donts

--- Course-1/Week-3/variables.c
+++ Course-1/Week-3/variables.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-    int balance; //DECLARATION
-    balance = 40;//DEFINITION
+    int balance = 40; //DECLARATION
+    
     printf("I have %d dollars in my account\n", balance);//USE
     return 0;
 }
