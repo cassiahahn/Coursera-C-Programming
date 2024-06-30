2f009f00c01e63b8c8fe0e51b715016021f02b85
Cassia <kklyra@gmail.com>
2024-06-30 19:40:13 +0200

    correction loop

--- Course-1/Week-3/loops.c
+++ Course-1/Week-3/loops.c
@@ -4,7 +4,7 @@ int main(void) {
     int numberOfHazelnuts = 0;
     int distanceTraveled = 0;
     for (i = 0; i < 9 ; i++) {
-        printf("At %d miles I have %d hazelnuts.\n, distanceTraveled, numberOfHazelnuts");
+        printf("At %d miles I have %d hazelnuts.\n", distanceTraveled, numberOfHazelnuts);
         distanceTraveled = distanceTraveled + 1;
         numberOfHazelnuts = numberOfHazelnuts + 3;
 
