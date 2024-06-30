aec73f6c37c47dd61d88a4ed427b901671f6fbf5
Cassia <kklyra@gmail.com>
2024-06-30 19:20:50 +0200

    Adding new names and variables

--- Course-1/Week-3/variables.c
+++ Course-1/Week-3/variables.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main() {
-    int balance; //DECLARATION
+    int balance;//DECLARATION
+    int Balance;//DECLARATION 2
     balance = 40;//DEFINITION
+    Balance = 30;
     printf("I have %d dollars in my account\n", balance);//USE
+    printf("I have %d dollars in my account\n", Balance);//USE
     return 0;
 }
