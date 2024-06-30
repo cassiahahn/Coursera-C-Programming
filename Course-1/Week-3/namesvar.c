63166b74dfa707de3931aa904bd5609f8b043b40
Cassia <kklyra@gmail.com>
2024-06-30 19:21:39 +0200

    Adding new names and variables

--- Course-1/Week-3/namesvar.c
+++ Course-1/Week-3/namesvar.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-int main(void) {
-    /* Variable names can use:  
-    lowercase and uppercase letters (characters) and digits
-    do not use special characters like @ # & " ...
-    do not use accented characters like é è à ...
-    do not start with a digit
-    start only with a letter
-    spaces are forbidden
-     _ may be used instead of a space in the name of the variable
-    YouCanUseUppercaseLettersBetweenWordsInsteadOfSpaces   */
-
-    int validBalance;
-    validBalance = 50;
-    printf("I have %d dollars in my bank account.\n", validBalance);
+int main() {
+    int balance;//DECLARATION
+    int Balance;//DECLARATION 2
+    balance = 40;//DEFINITION
+    Balance = 30;
+    printf("I have %d dollars in my account\n", balance);//USE
+    printf("I have %d dollars in my account\n", Balance);//USE
     return 0;
 }
