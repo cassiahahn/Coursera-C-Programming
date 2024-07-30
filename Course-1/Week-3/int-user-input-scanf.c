c070d04c3acadde0057a418e826ef26316eef73e
Cassia <kklyra@gmail.com>
2024-07-30 08:34:10 +0200

    Multiples variables

--- Course-1/Week-3/int-user-input-scanf.c
+++ Course-1/Week-3/int-user-input-scanf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main () {
     int age;//DECLARE
-    age = 17;//DEFINE
     printf("What is your age?\n");
     scanf("%d", &age);
     printf("You are %d years old\n", age);//USE
